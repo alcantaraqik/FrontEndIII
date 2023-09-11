692bb358771dfce92c40cd6d81d985c5ddc78723
Matheus Alcantara <99972273+alcantaraqik@users.noreply.github.com>
2023-09-11 19:39:55 -0300

    Update script.js

--- TED1/script.js
+++ TED1/script.js
@@ -1,12 +1,10 @@
 
-// Criar o array de objetos de produtos
 const produtos = [
     { nome: 'Geladeira', preco: 3000 },
     { nome: 'Maquina de Lavar', preco: 3400 },
     { nome: 'Panela', preco: 300 }
   ];
   
-  // Usar o método map() para aplicar um aumento de preço de 1.5 para cada produto
   const produtosComAumento = produtos.map(produto => {
     return {
       nome: produto.nome,
@@ -14,5 +12,4 @@ const produtos = [
     };
   });
   
-  // Exibir o resultado no console
   console.log(produtosComAumento);
